c9958edd5b345aee60662c6f66292aff304da644
FabrizioRamirezSanchez <fabrizio.ramirez.sanchez@vallegrande.edu.pe>
2026-07-12 22:10:14 -0500

    feature Hackaton final 0

--- frontend-app/src/environments/environment.prod.ts
+++ frontend-app/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
+  useLocalhost: false,
   apiUrl: {
     vehiculo: 'http://hct-vehiculo-ramirez-fabrizio-service.hct-vehiculo-ramirez-fabrizio.svc.cluster.local:8080/api/vehiculos',
     cliente: 'http://hct-cliente-ramirez-fabrizio-service.hct-cliente-ramirez-fabrizio.svc.cluster.local:8080/api/clientes',
--- frontend-app/src/environments/environment.ts
+++ frontend-app/src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
   apiUrl: {
-    vehiculo: 'http://localhost:8081/api/vehiculos',
-    cliente: 'http://localhost:8082/api/clientes',
-    alquiler: 'http://localhost:8083/api/alquileres'
+    vehiculo: '/api/vehiculos',
+    cliente: '/api/clientes',
+    alquiler: '/api/alquileres'
   }
 };
